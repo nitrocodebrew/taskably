d71625d92199847e5936c8b035678b4a4b63992c
John Smicklo <204639360+nitrocodebrew@users.noreply.github.com>
2025-08-31 11:59:25 -0400

    Fix TaskListSummary bug

--- src/components/TaskListSummary.jsx
+++ src/components/TaskListSummary.jsx
@@ -1,10 +1,15 @@
 function TaskListSummary({ totalTasks }) {
     const properGrammar = (totalTasks) => {
-        if(totalListItems === 1)
+        if(totalTasks === 1)
             return <p>There is currently {totalTasks} task.</p>;
         else
             return <p>There are currently {totalTasks} tasks.</p>;
     };
+    return (
+        <>
+            {properGrammar(totalTasks)}
+        </>
+    )
 }
 
 export default TaskListSummary
