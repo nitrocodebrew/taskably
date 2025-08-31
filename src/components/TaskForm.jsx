e0a1f811487781a4452874db8fac0abcb7fa76d1
John Smicklo <204639360+nitrocodebrew@users.noreply.github.com>
2025-08-31 11:44:28 -0400

    Initial TaskForm declaration

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+function TaskForm({ taskList, setTaskList }) {
+    const [input, setInput] = useState('');
+
+    // For dev environment
+    useEffect(() => console.log(taskList), [taskList]);
+
+    const preventDefaultSubmit = e => e.preventDefault();
+    const handleInputChange = e => setInput(e.target.value);
+    const handleCreateTask = () => {
+        const newTask = input.trim();
+
+        // Require at least 4 characters per task label.
+        if(newTask !== '' && newTask.length >= 4) {
+            // Save the new task.
+            setTaskList([
+                ...taskList,
+                {
+                    id: new Date().getTime(),
+                    label: newTask,
+                    done: false
+                }
+            ]);
+        }
+        else {
+            // TO-DO: Make this more elegant
+            alert('The task label should contain at least four (4) characters; try again.');
+            return;
+        }
+    };
+
+    return (
+        <form onSubmit={preventDefaultSubmit}>
+            <input type="text" value={input} onChange={handleInputChange} />
+            <input type="submit" value="Add +" onClick={handleCreateTask} />
+        </form>
+    );
+}
+
+export default TaskForm
